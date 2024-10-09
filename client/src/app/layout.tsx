e90dff234fcd20fa241c9054f41d28f021a3cea4
mvoemel <michael@voemel.org>
2024-10-09 16:05:37 +0200

    FEAT(client): added biolerplate code for client-react

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+
+import { ThemeProvider } from "@/providers/theme-provider";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Studyflow",
+  description: "Create your personalized study plan",
+};
+
+const font = Nunito({ subsets: ["latin"] });
+
+type RootLayoutProps = Readonly<{
+  children: React.ReactNode;
+}>;
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
