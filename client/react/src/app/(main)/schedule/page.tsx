b9882a71ff8f30862fe35f13e2c38b50416050cd
shpetim <veselshp@students.zhaw.ch>
2024-10-10 20:21:02 +0200

    chore: implemeneted the dayView of the calendar in dashboard and added calendar into schedule

--- client/react/src/app/(main)/schedule/page.tsx
+++ client/react/src/app/(main)/schedule/page.tsx
@@ -1,6 +1,47 @@
 // TODO: implement
+"use client"
+import { useEffect, useState } from 'react';
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from '@fullcalendar/interaction'
+
+
 const SchedulePage = () => {
-  return <div>SchedulePage</div>;
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const response = await fetch('/api/events');
+            const data = await response.json();
+            setEvents(data);
+        };
+
+        fetchEvents();
+    }, []);
+
+  return (
+
+      <FullCalendar
+          plugins={[ timeGridPlugin,interactionPlugin ]}
+          initialView={"timeGridWeek"}
+          weekends={false}
+          nowIndicator={true}
+          events={events}
+          slotMinTime={'08:00:00'}
+          slotMaxTime={'20:00:00'}
+          slotLabelFormat={{
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: false
+          }}
+          eventTimeFormat={{
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: false
+          }}
+          editable={true}
+      />
+  );
 };
 
 export default SchedulePage;
