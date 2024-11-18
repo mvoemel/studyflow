14688f6d5058a33d398db74c587f4e2b5f5a42f8
mvoemel <michael@voemel.org>
2024-11-18 17:30:03 +0100

    FIX: fixed ts error

--- client/src/components/dialogs/createSchedule.tsx
+++ client/src/components/dialogs/createSchedule.tsx
@@ -1,25 +1,33 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogDescription
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
 } from "@/components/ui/dialog";
 import { CreateScheduleForms } from "@/components/dialogforms/createScheduleForms";
+import { Module } from "@/context/ModuleContext";
 
-export function CreateScheduleDialog({ isOpen, onClose, modules }: { isOpen: boolean, onClose: () => void, modules: any[] }) {
-
-    return (
-        <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent className="max-w-[50%] max-h-[90%] overflow-auto">
-                <DialogHeader>
-                    <DialogTitle>Generate a Study plan</DialogTitle>
-                    <DialogDescription>
-                        Fill in all the details to generate a study plan.
-                    </DialogDescription>
-                </DialogHeader>
-                <CreateScheduleForms onClose={onClose} modules={modules} />
-            </DialogContent>
-        </Dialog>
-    );
+export function CreateScheduleDialog({
+  isOpen,
+  onClose,
+  modules,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  modules: Module[];
+}) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="max-w-[50%] max-h-[90%] overflow-auto">
+        <DialogHeader>
+          <DialogTitle>Generate a Study plan</DialogTitle>
+          <DialogDescription>
+            Fill in all the details to generate a study plan.
+          </DialogDescription>
+        </DialogHeader>
+        <CreateScheduleForms onClose={onClose} modules={modules} />
+      </DialogContent>
+    </Dialog>
+  );
 }
