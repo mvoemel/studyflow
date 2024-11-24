2c25d3b06996f6486c6a2664fbaef2bad779498d
shpetim <veselshp@students.zhaw.ch>
2024-11-24 17:46:13 +0100

    fix: fixed some merge conflicts

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -1,10 +1,10 @@
 import {
     SidebarProvider,
     SidebarInset,
-} from "@/components/sidebar/sidebar"
+} from "@/components/ui/sidebar"
 import { ReactNode } from "react";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { DegreeProvider } from "@/context/DegreeContext";
+import { DegreeProvider } from "@/context/degree-context";
 import { ModuleProvider } from "@/context/ModuleContext";
 import AppSidebarHeader from "@/components/sidebar/app-sidebar-header";
 
--- client/src/app/(main)/schedule/page.tsx
+++ client/src/app/(main)/schedule/page.tsx
@@ -3,7 +3,7 @@ import {useCallback, useEffect, useState} from 'react';
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from '@fullcalendar/interaction'
-import { useDegree } from '@/context/DegreeContext';
+import { useDegree } from '@/context/degree-context';
 import { useModule } from '@/context/ModuleContext';
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -52,28 +52,39 @@ const SchedulePage = () => {
     );
 
   return (
-    <main className="">
-      <FullCalendar
-        plugins={[timeGridPlugin, interactionPlugin]}
-        initialView={"timeGridWeek"}
-        weekends={false}
-        nowIndicator={true}
-        events={events}
-        slotMinTime={"08:00:00"}
-        slotMaxTime={"20:00:00"}
-        slotLabelFormat={{
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        }}
-        eventTimeFormat={{
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        }}
-        editable={true}
-      />
-    </main>
+      <div
+          className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+          <h1 className="text-3xl font-semibold">Schedule Plan</h1>
+          <div className="flex gap-4">
+              <Button onClick={openAddAppointmentDialog}>Add an Appointment</Button>
+              <Button onClick={openCreateScheduleDialog}>Create a Schedule Plan</Button>
+          </div>
+          <Card className="p-7">
+              <FullCalendar
+                  plugins={[timeGridPlugin, interactionPlugin]}
+                  initialView={"timeGridWeek"}
+                  weekends={false}
+                  nowIndicator={true}
+                  events={events}
+                  slotMinTime={'08:00:00'}
+                  slotMaxTime={'20:00:00'}
+                  slotLabelFormat={{
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: false
+                  }}
+                  eventTimeFormat={{
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: false
+                  }}
+                  editable={true}
+              />
+          </Card>
+          <AddAppointmentDialog isOpen={isAddAppointmentDialogOpen} onClose={closeAddAppointmentDialog}/>
+          <CreateScheduleDialog isOpen={isCreateScheduleDialogOpen} onClose={closeCreateScheduleDialog}
+                                modules={filteredModules}/>
+      </div>
   );
 };
 
