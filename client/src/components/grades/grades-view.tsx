d5d4e27c4b363ad76e9526719171f01c8fbc4c67
mvoemel <michael@voemel.org>
2024-11-27 23:17:30 +0100

    REFACTOR: quick unstable refactor of GradesView component

--- client/src/components/grades/grades-view.tsx
+++ client/src/components/grades/grades-view.tsx
@@ -2,30 +2,31 @@
 
 import { useState } from "react";
 import { ChevronDown } from "lucide-react";
-import { degrees } from "./mock-data";
-import { Degree, Semester, Module } from "./types";
+import { semesterGradesMock } from "./mock-data";
+import { Semester, Module } from "./types";
 import clsx from "clsx";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 
-// TODO: reafactor this component and split into smaller components
+// TODO: refactor this component and split into smaller components
+// TODO: properly implement types
 
 const GradesView = () => {
   return (
     <Card className="bg-muted/50 text-sm min-w-full max-w-5xl mx-auto p-4 rounded-lg">
       <div className="grid grid-cols-2 mb-2 px-4">
-        <span>Name</span>
+        <span>Semester</span>
         <span className="text-right">ECTS & Grade</span>
       </div>
       <div className="space-y-2">
-        {degrees.map((degree, index) => {
+        {semesterGradesMock.map((semester, index) => {
           // if is last return without separator
-          if (index === degrees.length - 1)
-            return <DegreeSection key={index} degree={degree} />;
+          if (index === semesterGradesMock.length - 1)
+            return <SemesterSection key={index} semester={semester} />;
 
           return (
             <div key={index}>
-              <DegreeSection degree={degree} />
+              <SemesterSection semester={semester} />
               <Separator />
             </div>
           );
@@ -35,7 +36,7 @@ const GradesView = () => {
   );
 };
 
-const DegreeSection = ({ degree }: { degree: Degree }) => {
+const SemesterSection = ({ semester }: { semester: Semester }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
@@ -51,22 +52,20 @@ const DegreeSection = ({ degree }: { degree: Degree }) => {
           }`}
         />
         <div className="flex justify-between items-center w-full">
-          <span>{degree.name}</span>
+          <span>{semester.name}</span>
           <span className="flex items-center">
             <span className="mr-4 text-muted-foreground text-xs">
-              {degree.totalEcts}/{degree.totalEcts}
+              {semester.ects}/{semester.ects}
             </span>
             <span
               className={clsx("", {
-                "text-green-500": degree.totalGrade >= 5,
-                "text-yellow-500":
-                  degree.totalGrade >= 4.5 && degree.totalGrade < 5,
-                "text-orange-500":
-                  degree.totalGrade >= 4 && degree.totalGrade < 4.5,
-                "text-red-500": degree.totalGrade < 4,
+                "text-green-500": semester.grade >= 5,
+                "text-yellow-500": semester.grade >= 4.5 && semester.grade < 5,
+                "text-orange-500": semester.grade >= 4 && semester.grade < 4.5,
+                "text-red-500": semester.grade < 4,
               })}
             >
-              {degree.totalGrade}
+              {semester.grade}
             </span>
           </span>
         </div>
@@ -76,55 +75,17 @@ const DegreeSection = ({ degree }: { degree: Degree }) => {
           isExpanded ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
         }`}
       >
-        {degree.semesters.map((semester, index) => (
-          <SemesterSection key={index} semester={semester} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const SemesterSection = ({ semester }: { semester: Semester }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  return (
-    <div className="border-none">
-      <button
-        onClick={() => setIsExpanded(!isExpanded)}
-        className="w-full p-4 rounded-md hover:bg-muted/50 flex flex-row items-center text-left"
-        aria-expanded={isExpanded}
-      >
-        <ChevronDown
-          className={`h-4 w-4 mr-2 shrink-0 transition-transform duration-2000 ${
-            isExpanded ? "rotate-180" : ""
-          }`}
-        />
-        <div className="flex justify-between items-center w-full">
-          <span>{semester.name}</span>
-          <span className="flex items-center">
-            <span className="mr-4 text-muted-foreground text-xs">
-              {semester.ects}
-            </span>
-            <span>{semester.grade}</span>
-          </span>
-        </div>
-      </button>
-      <div
-        className={`mt-1 space-y-1 pl-4 overflow-hidden transition-all duration-200 ${
-          isExpanded ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
-        }`}
-      >
         {semester.modules.map((module, index) => (
-          <ModuleRow key={index} module={module} />
+          <ModuleSection key={index} module={module} />
         ))}
       </div>
     </div>
   );
 };
 
-const ModuleRow = ({ module }: { module: Module }) => {
+const ModuleSection = ({ module }: { module: Module }) => {
   return (
-    <div className="border-[1.5px] bg-opacity-50 p-4 rounded-md hover:bg-muted/50 flex justify-between items-center">
+    <div className="bg-opacity-50 p-4 rounded-md hover:bg-muted/50 flex justify-between items-center">
       <span>{module.name}</span>
       <span className="flex items-center">
         <span className="mr-4 text-muted-foreground text-xs">
--- client/src/components/grades/mock-data.ts
+++ client/src/components/grades/mock-data.ts
@@ -1,50 +1,38 @@
+import { Semester } from "@/types";
 import { Degree } from "./types";
 
-const degrees: Degree[] = [
+const semesterGradesMock: Semester[] = [
   {
-    name: "Master Computer Science",
-    totalEcts: 60,
-    totalGrade: 5.3,
-    semesters: [
+    name: "1. Semester",
+    ects: 30,
+    grade: 5.5,
+    modules: [
+      { name: "Programming 1", ects: 15, grade: 5.6 },
+      { name: "Linear Algebra", ects: 15, grade: 5.4 },
+    ],
+  },
+  {
+    name: "2. Semester",
+    ects: 30,
+    grade: 5.1,
+    modules: [
+      { name: "Programming 2", ects: 15, grade: 5 },
       {
-        name: "1. Semester",
-        ects: 30,
-        grade: 5.5,
-        modules: [
-          { name: "Programming 1", ects: 15, grade: 5.6 },
-          { name: "Linear Algebra", ects: 15, grade: 5.4 },
-        ],
-      },
-      {
-        name: "2. Semester",
-        ects: 30,
-        grade: 5.1,
-        modules: [
-          { name: "Programming 2", ects: 15, grade: 5 },
-          {
-            name: "System Design and Computer Architecture",
-            ects: 15,
-            grade: 5.2,
-          },
-        ],
-      },
-      {
-        name: "3. Semester",
-        ects: 30,
-        grade: "-",
-        modules: [
-          { name: "Algorithms and Datastructures", ects: 15, grade: "-" },
-          { name: "Software Development 1", ects: 15, grade: "-" },
-        ],
+        name: "System Design and Computer Architecture",
+        ects: 15,
+        grade: 5.2,
       },
     ],
   },
   {
-    name: "Bachelor Computer Science",
-    totalEcts: 180,
-    totalGrade: 4.9,
-    semesters: [],
+    name: "3. Semester",
+    ects: 30,
+    grade: "-",
+    modules: [
+      { name: "Algorithms and Datastructures", ects: 15, grade: "-" },
+      { name: "Software Development 1", ects: 15, grade: "-" },
+    ],
   },
 ];
 
-export { degrees };
+export { semesterGradesMock };
