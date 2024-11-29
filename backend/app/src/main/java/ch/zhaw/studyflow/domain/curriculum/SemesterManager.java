eabf385ad0a71f215af03a6181335e34c64fa427
shpetim <veselshp@students.zhaw.ch>
2024-11-29 18:32:01 +0100

    chore: first implementation of semester logic and endpoints

--- backend/app/src/main/java/ch/zhaw/studyflow/domain/curriculum/SemesterManager.java
+++ backend/app/src/main/java/ch/zhaw/studyflow/domain/curriculum/SemesterManager.java
@@ -1,12 +1,13 @@
 package ch.zhaw.studyflow.domain.curriculum;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SemesterManager {
 
-    void createSemester(Semester semester);
+    void createSemester(Semester semester, long degreeId, long userId);
     List<Semester> getSemestersForStudent(long userId);
-    Semester getSemesterById(long semesterId);
+    Optional<Semester> getSemesterById(long semesterId);
     void updateSemester(Semester semester);
     void deleteSemester(long semesterId);
 
--- backend/app/src/main/java/ch/zhaw/studyflow/domain/curriculum/imps/SemesterManagerImpl.java
+++ backend/app/src/main/java/ch/zhaw/studyflow/domain/curriculum/imps/SemesterManagerImpl.java
@@ -2,32 +2,41 @@ package ch.zhaw.studyflow.domain.curriculum.imps;
 
 import ch.zhaw.studyflow.domain.curriculum.Semester;
 import ch.zhaw.studyflow.domain.curriculum.SemesterManager;
+import ch.zhaw.studyflow.services.persistence.SemesterDao;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SemesterManagerImpl implements SemesterManager {
-    @Override
-    public void createSemester(Semester semester) {
 
+    private final SemesterDao SemesterDao;
+
+    public SemesterManagerImpl(SemesterDao semesterDao) {
+        this.SemesterDao = semesterDao;
+    }
+
+    @Override
+    public void createSemester(Semester semester, long degreeId, long userId) {
+        SemesterDao.createSemester(semester, degreeId, userId);
     }
 
     @Override
     public List<Semester> getSemestersForStudent(long userId) {
-        return List.of();
+        return SemesterDao.getSemestersForStudent(userId);
     }
 
     @Override
-    public Semester getSemesterById(long semesterId) {
-        return null;
+    public Optional<Semester> getSemesterById(long semesterId) {
+        return SemesterDao.getSemesterById(semesterId);
     }
 
     @Override
     public void updateSemester(Semester semester) {
-
+        SemesterDao.updateSemester(semester);
     }
 
     @Override
     public void deleteSemester(long semesterId) {
-
+        SemesterDao.deleteSemester(semesterId);
     }
 }
