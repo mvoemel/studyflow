2b6958ab23104b786f79aadea3f67efbcb7a5641
Tobïas Kugel <kugeltob@students.zhaw.ch>
2024-12-08 17:02:32 +0100

    add cookie parsing test and improve parsing

--- backend/app/src/test/java/ch/zhaw/studyflow/webserver/http/cookies/CookieTest.java
+++ backend/app/src/test/java/ch/zhaw/studyflow/webserver/http/cookies/CookieTest.java
@@ -61,4 +61,26 @@ class CookieTest {
         Optional<Cookie> cookieOpt = Cookie.readFromHeader(header);
         assertFalse(cookieOpt.isPresent());
     }
+
+    @Test
+    void testCookieParsing() {
+        String header = "name=value; Expires=Wed, 21 Oct 2015 07:28:00 GMT; Max-Age=3600; Domain=example.com; Path=/test; Secure; HttpOnly; SameSite=Strict";
+        Optional<Cookie> cookieOpt = Cookie.readFromHeader(header);
+        assertTrue(cookieOpt.isPresent());
+        Cookie cookie = cookieOpt.get();
+        assertEquals("name", cookie.getName());
+        assertEquals("value", cookie.getValue());
+        assertEquals(3600, cookie.getMaxAge());
+        assertTrue(cookie.isSecure());
+        assertEquals("example.com", cookie.getDomain());
+        assertEquals("/test", cookie.getPath());
+        assertEquals(SameSitePolicy.STRICT, cookie.getSameSite());
+        assertEquals(LocalDateTime.of(2015, 10, 21, 7, 28), cookie.getExpires());
+    }
+
+    @Test
+    void testMissingArgument() {
+        String header = "name=value; Expires=Wed, 21 Oct 2015 07:28:00 GMT; Max-Age=; Domain=example.com; Path=/test; Secure; HttpOnly";
+        assertThrows(IllegalArgumentException.class, () -> Cookie.readFromHeader(header));
+    }
 }
